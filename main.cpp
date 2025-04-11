8c9bfeb2196fd804e19975c8ae822ded3c5cc06c
Ana Vi <dismal.damsel@gmail.com>
2025-04-11 13:08:11 +0300

    Initial commit with C++ files and basic password manager functionality

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "password_manager.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ password_manager.h
@@ -0,0 +1,12 @@
+#ifndef PASSWORD_MANAGER_H
+#define PASSWORD_MANAGER_H
+
+#include <string>
+
+void showMenu();
+void addPassword();
+void viewPasswords();
+void deletePassword();
+void savePasswords();
+
+#endif
